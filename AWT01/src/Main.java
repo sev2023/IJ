72a531c78e3c24a448d328c6f03341ca9b8c3a54
sev2023 <19euro@gmail.com>
2023-03-01 10:04:54 +0000

    AWT or Swing?

--- AWT01/src/Main.java
+++ AWT01/src/Main.java
@@ -10,9 +10,20 @@ public class Main {
         // Swing JFrame setup
         JFrame myJF = new JFrame();
 
-        myJF.setSize(400,300);
+        myJF.setSize(400,500);
         myJF.setDefaultCloseOperation(3);
         myJF.setLayout(null);
+        myJF.setResizable(false);
         myJF.setVisible(true);
+        myJF.getContentPane().setBackground(new Color(107, 134, 151));
+
+        // Label to hold calc display
+        JLabel JLdisplay = new JLabel("...0");
+        JLdisplay.setBounds(50,20,300,30);
+        //JLdisplay.setOpaque(true);
+        JLdisplay.setBackground(new Color(177, 188, 173));
+
+        // Add components to Frame
+        myJF.add(JLdisplay);
     }
 }
