c5b3377b592b13bd12d545169759e33b9516a3e5
sev2023 <19euro@gmail.com>
2023-03-01 09:48:15 +0000

    AWT or Swing?

---
+++ AWT01/src/AWT.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+public class AWT {
+    //CONSTRUCTOR
+    public AWT() {
+        Frame calc = new Frame();
+        calc.setSize(300, 600);
+        calc.setVisible(true);
+
+    }
+}
--- AWT01/src/Main.java
+++ AWT01/src/Main.java
@@ -1,7 +1,18 @@
+import java.awt.*;
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args)
     {
+        //System.out.println("Hello world!");
+        //AWT myAwt = new AWT();
 
-        System.out.println("Hello world!");
+        // Swing JFrame setup
+        JFrame myJF = new JFrame();
+
+        myJF.setSize(400,300);
+        myJF.setDefaultCloseOperation(3);
+        myJF.setLayout(null);
+        myJF.setVisible(true);
     }
 }
