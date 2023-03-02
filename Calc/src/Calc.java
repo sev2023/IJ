b7c0ef05621f5a7ffefd40a431dca2406136d0e4
sev2023 <19euro@gmail.com>
2023-03-02 23:19:55 +0000

    new way of maknig JFrame

---
+++ Calc/src/Calc.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Calc {
+    public static void main(String[] args){
+
+        MyJFrame mm = new MyJFrame();
+//        EventQueue.invokeLater(new Runnable() {
+//            @Override
+//            public void run() {
+//                new MyJFrame();
+//            }
+//        });
+    }
+}
---
+++ Calc/src/MyJFrame.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MyJFrame extends JFrame {
+    // CONSTRUCTOR
+    public MyJFrame(){
+        super("This is title..");
+        setDefaultCloseOperation(3);
+        setSize(300,300);
+        setVisible(true);
+    }
+}
