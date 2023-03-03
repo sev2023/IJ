8577326ea7a3ac00c7047a51cbd144531d2e29cc
sev2023 <19euro@gmail.com>
2023-03-03 17:11:58 +0000

    DONE!! Ready to finish

--- pjwstk/src/Main.java
+++ pjwstk/src/Main.java
@@ -4,23 +4,38 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Main extends JFrame implements ActionListener{
-    @Override
+    @Override // this could be in separate class
     public void actionPerformed(ActionEvent e){
-        form.setSize(200,200);
+        s1 = e.getActionCommand() + s1;
+        display.setText(s1);
+
     }
 
     // this is static accessible from everywhere
     static JFrame form;
     static JTextField display;
+    static String s1 = "";
     public static void main(String[] args)
     {
-        Main m = new Main(); // made only to access ActionListener
         form = new JFrame("Main"); // this is static already declared
+        display = new JTextField(20);
+
+        form.add(display);
 
         var btn1 = new JButton("1");
-        btn1.addActionListener(m);
+        var btn2 = new JButton("2");
+        var btn3 = new JButton("3");
+
+        btn1.addActionListener(new Main());
+        btn2.addActionListener(new Main());
+        btn3.addActionListener(new Main());
+
         form.add(btn1);
+        form.add(btn2);
+        form.add(btn3);
+
         form.setSize(300,300);
+        form.setLayout(new FlowLayout());
         form.setVisible(true);
     }
 }
